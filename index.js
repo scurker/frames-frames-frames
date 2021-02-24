3bf47651cfba439ce00e1d02c78426a6aa6a5788
Jason <jason@scurker.com>
2021-02-24 12:19:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const topShadowRoot = document.getElementById('top-shadow-root')
+const nestedShadowRoot = document.getElementById('nested-shadow-root')
+const shadowStyles = `
+  .color-contrast {
+    background-color: #f00;
+    border: 1px solid #a00;
+    color: pink;
+    padding: .5em 1em;
+    border-radius: 4px;
+  }
+`
+
+if (topShadowRoot) {
+  const shadow = topShadowRoot.attachShadow({ mode: 'open' })
+  const style = document.createElement('style')
+  style.innerText = shadowStyles
+  shadow.innerHTML = `
+    <h2>Shadow Dom</h2>
+    <button class="color-contrast">Click Me</button>
+    <h3>Shadow Frame (same-domain)</h3>
+    <iframe title="shadow nested frame (same-domain)" src="nested-frame.html"></iframe>
+    <h3>Shadow Frame (x-domain)</h3>
+    <iframe title="shadow nested frame (x-domain)" src="nested-frame.html"></iframe>
+  `
+  shadow.appendChild(style)
+}
+
+if (nestedShadowRoot) {
+  const shadow = nestedShadowRoot.attachShadow({ mode: 'open' })
+  const style = document.createElement('style')
+  style.innerText = shadowStyles
+  shadow.innerHTML = `
+    <h2>Nested Shadow Dom</h2>
+    <button class="color-contrast">Click Me</button>
+  `
+  shadow.appendChild(style)
+}
